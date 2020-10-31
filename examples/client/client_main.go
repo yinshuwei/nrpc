b1388bb0f374a7082d12552e0115ca5da05767d6
yinshuwei <yinshuwei@foxmail.com>
2020-10-31 17:51:37 +0800

    all

--- examples/client/client_main.go
+++ examples/client/client_main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"log"
+	"net"
 
 	"github.com/yinshuwei/nrpc"
 )
 
 func main() {
-	cli, err := nrpc.Dial("tcp", "127.0.0.1:8003")
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
 	if err != nil {
 		log.Println(err)
 	}
+
+	cli := nrpc.NewClient(conn)
 
 	for i := 0; i < 100; i++ {
 		reply := map[string]int{}
